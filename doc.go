e4752170561dd5c3643cda35f5afb80c47174bde
MarvinJWendt <git@marvinjwendt.com>
2022-07-08 17:13:47 +0200

    updated docs

--- doc.go
+++ doc.go
@@ -2,5 +2,12 @@
 Package ghissue enables your users to submit issues to GitHub directly.
 
 ![Demo Video](https://raw.githubusercontent.com/atomicgo/ghissue/main/demo.gif)
+
+Example:
+
+	repo := ghissue.NewRepository("atomicgo", "ghissue")
+	// [...]
+	err := errors.New("This is an error")
+	repo.CreateErrorReport(err) // Only creates an error report if the error is not nil
 */
 package ghissue
